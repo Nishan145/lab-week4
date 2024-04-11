53aca5107762b0ccbaa785bffc779ae6c221cbf8
Nik <Tilija_nik@hotmail.co.uk>
2024-04-11 10:01:05 +0100

    made a skeleton structure

--- server/server.js
+++ server/server.js
@@ -3,3 +3,12 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+//listen
+app.listen(8080, function () {
+  console.log("Server is listening to 8080...");
+});
+//app.get root route
+app.get("/", function (request, response) {
+  response.json({ message: "Hello World!" });
+});
