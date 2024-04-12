146590bf0020ff777066e568528074bf7e202553
Nik <Tilija_nik@hotmail.co.uk>
2024-04-12 11:58:18 +0100

    trying to work out fetch

--- client/script.js
+++ client/script.js
@@ -5,10 +5,16 @@ function handleSubmit(event) {
   const username = event.target.username.value;
   const message = event.target.message.value;
   console.log({ username, message });
+
+  //post to message end point
   fetch("https://lab-week4-server.onrender.com/message", {
     method: "POST",
     body: JSON.stringify({ username, message }),
+    //
     headers: { "Content-Type": "application/json" },
   });
 }
 form.addEventListener("submit", handleSubmit);
+
+//
+fetch("https://lab-week4-server.onrender.com/", { method: "GET" });
